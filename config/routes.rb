00eea99048b01554fc08fbfce39109755b80d70e
falaziz <muhamma2@andrew.cmu.edu>
2020-04-12 16:10:01 +0000

    routes added for new controlllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   resources :bands
   resources :band_genres
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
+  resources :sessions
+  get 'user/edit' => 'users#edit', :as => :edit_current_user
+  get 'signup' => 'users#new', :as => :signup
+  get 'login' => 'sessions#new', :as => :login
+  get 'logout' => 'sessions#destroy', :as => :logout
+
+  # Default route
+  root :to => 'bands#index', :as => :home
 end
